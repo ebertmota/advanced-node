1ba783b0bc90c4c401b43f564e6d8f0c7d6e6151
ebertmota <ebert.mota03@gmail.com>
2021-11-23 09:12:16 -0300

    ensure service returns an AccessToken on success

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -38,7 +38,9 @@ describe('FacebookAuthenticationService', () => {
     userAccountRepo.saveWithFacebook.mockResolvedValue({
       id: 'any_account_id',
     });
-    crypto = mock();
+    crypto = mock<TokenGenerator>({
+      generateToken: jest.fn(() => Promise.resolve('any_generated_token')),
+    });
     sut = new FacebookAuthenticationService(
       facebookApi,
       userAccountRepo,
@@ -105,5 +107,13 @@ describe('FacebookAuthenticationService', () => {
       expirationInMs: AccessToken.expirationInMs,
     });
     expect(crypto.generateToken).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return an AccessToken on success', async () => {
+    const authResult = await sut.perform({
+      token,
+    });
+
+    expect(authResult).toEqual(new AccessToken('any_generated_token'));
   });
 });
