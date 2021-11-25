d76e6a7aa7aa70f37c25548d3b046f22e68d2d69
ebertmota <ebert.mota03@gmail.com>
2021-11-25 09:21:43 -0300

    refactor: add missing interface to class implementations

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { HttpGetClient } from '.';
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get(agrs: HttpGetClient.Params): Promise<any> {
     const result = await axios.get(agrs.url, { params: agrs.params });
 
