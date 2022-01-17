59d886424b6368f077be53777f93d9256556c35a
ebertmota <ebert.mota03@gmail.com>
2022-01-17 09:40:08 -0300

    ensure FacebookLoginController returns 401 if authentication fails

--- tests/application/facebook-login.spec.ts
+++ tests/application/facebook-login.spec.ts
@@ -1,5 +1,6 @@
 import { mock, MockProxy } from 'jest-mock-extended';
 import { FacebookAuthentication } from '@/domain/features';
+import { AuthenticationError } from '@/domain/errors';
 
 type HttpResponse = { statusCode: number; data: any };
 
@@ -9,12 +10,19 @@ class FacebookLoginController {
   ) {}
 
   async handle(httpRequest: any): Promise<HttpResponse> {
-    await this.facebookAuthentication.perform({
+    if (!httpRequest.token) {
+      return {
+        statusCode: 400,
+        data: new Error('The field token is required'),
+      };
+    }
+
+    const result = await this.facebookAuthentication.perform({
       token: httpRequest.token,
     });
     return {
-      statusCode: 400,
-      data: new Error('The field token is required'),
+      statusCode: 401,
+      data: result,
     };
   }
 }
@@ -65,5 +73,16 @@ describe('FacebookLoginController', () => {
       token: 'any_token',
     });
     expect(facebookAuth.perform).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return 401 if authentication fails', async () => {
+    const error = new AuthenticationError();
+    facebookAuth.perform.mockResolvedValueOnce(error);
+    const result = await sut.handle({ token: 'any_token' });
+
+    expect(result).toEqual({
+      statusCode: 401,
+      data: error,
+    });
   });
 });
