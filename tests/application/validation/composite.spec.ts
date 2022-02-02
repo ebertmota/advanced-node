44759689adfc97dcbe433d10803954a1dfaf8aa0
ebertmota <ebert.mota03@gmail.com>
2022-02-02 09:47:35 -0300

    test: ensure ValidationComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,27 +1,56 @@
-import { mock } from 'jest-mock-extended';
+/* eslint-disable consistent-return */
+/* eslint-disable no-unreachable-loop */
+/* eslint-disable no-restricted-syntax */
+import { mock, MockProxy } from 'jest-mock-extended';
 
 interface Validator {
   validate: () => Error | undefined;
 }
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor(private readonly validators: Validator[]) {}
 
-  validate(): undefined {
-    return undefined;
+  validate(): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate();
+
+      if (error !== undefined) {
+        return error;
+      }
+    }
   }
 }
 
 describe('ValidationComposite', () => {
-  it('should return undefined if all Validators return undefined', () => {
-    const validator1 = mock<Validator>();
+  let sut: ValidationComposite;
+  let validator1: MockProxy<Validator>;
+  let validator2: MockProxy<Validator>;
+  let validators: Validator[];
+
+  beforeAll(() => {
+    validator1 = mock<Validator>();
     validator1.validate.mockReturnValue(undefined);
-    const validator2 = mock<Validator>();
+    validator2 = mock<Validator>();
     validator2.validate.mockReturnValue(undefined);
-    const validators = [validator1, validator2];
-    const sut = new ValidationComposite(validators);
+    validators = [validator1, validator2];
+  });
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators);
+  });
+
+  it('should return undefined if all Validators return undefined', () => {
+    const error = sut.validate();
+
+    expect(error).toBe(undefined);
+  });
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValueOnce(new Error('error 1'));
+    validator2.validate.mockReturnValueOnce(new Error('error 2'));
 
     const error = sut.validate();
-    expect(error).toBe(undefined);
+
+    expect(error).toEqual(new Error('error 1'));
   });
 });
