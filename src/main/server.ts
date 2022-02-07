326e9c05c3a3189ca3ecda2231e54ecc85339de5
ebertmota <ebert.mota03@gmail.com>
2022-02-07 09:52:38 -0300

    feat: connect to postgres database before server starts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+
 import { env } from './config';
 import { app } from './config/app';
+import { config } from '../infra/postgres/helpers';
 
 import './config/module.alias';
 
-app.listen(env.appPort, () =>
-  console.log(`Sever running at http://localhost:${env.appPort}`),
-);
+createConnection(config)
+  .then(() => {
+    app.listen(env.appPort, () =>
+      console.log(`Sever running at http://localhost:${env.appPort}`),
+    );
+  })
+  .catch(console.error);
