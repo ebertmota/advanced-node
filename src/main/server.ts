6ef36bd508343404f5a6b3e9d6e7d0ac1c4621a8
ebertmota <ebert.mota03@gmail.com>
2022-02-08 09:25:14 -0300

    refactor: move postres connection settings to ormConfig.json

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,11 +4,10 @@ import { createConnection } from 'typeorm';
 
 import { env } from './config';
 import { app } from './config/app';
-import { config } from '../infra/postgres/helpers';
 
 import './config/module.alias';
 
-createConnection(config)
+createConnection()
   .then(() => {
     app.listen(env.appPort, () =>
       console.log(`Sever running at http://localhost:${env.appPort}`),
