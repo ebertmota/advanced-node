4d3e5135ee2bbe83c62ee439e9a44fcb89a81b16
ebertmota <ebert.mota03@gmail.com>
2022-02-10 09:06:05 -0300

    refactor: change fb token

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,7 @@ export const env = {
     clientSecret:
       process.env.FB_CLIENT_SECRET ?? '2e0e10c07a5a1d3618dcc85878d0924d',
     accessToken:
-      'EAAJHA0yw77EBADui5wAsfsI3c0axSOGwi1Q6FpZCTwjbZCrtIpVyCL2Qdp66sKA0bWqmaLrxcOLIGMEVJkdhYdak3XOrUmYo9FlEpmNO4UsKleELRbV2fkyEPZAXWbukwwhUm12Ys8pP6bp4mr3K8ZBkFa9jTs9kfVN9gZACwGb9WLGZCGoezVsMYpRHWSv7aVJZAhtbD5WzbaMVB5UatPq',
+      'EAAJHA0yw77EBABSqaEQKQr1SCZCRiii0my3anW9LHeHZCXDmNC1MRTjDCybL4m7UYze4azmD6hKZA1su7e0GBTQkNH9382tHHG3XZCnVwub5fnzx0J1J0ZCZBNGgd9E81Eux3daJRrLKn0v0VKRQEg4BQzC2JVc1MTXsWQZAfYod5Cq7JrSOdPjOo35YMsYZAIqOFCSWFK1ISyhikXQB8FBjfhN7n6ErN0NDdl7yL971dHxEa0oJcpc9ZCKu7Xp2jabYZD',
   },
   appPort: process.env.APP_PORT ?? 3333,
   jwtSecret: process.env.JWT_SECRET || 'ajdlaksdjlaskjd',
