b1206fcbd6d2b0ae2a9e733dddb6a491de61dcbe
ebertmota <ebert.mota03@gmail.com>
2022-02-14 09:53:07 -0300

    refactor: move production code to their own files

---
+++ src/application/middlewares/authentication.ts
@@ -0,0 +1,40 @@
+import { Authorize } from '@/domain/use-cases';
+import { forbidden, HttpResponse, ok } from '../helpers';
+import { RequiredStringValidator } from '../validation';
+
+type Request = {
+  authorization: string;
+};
+
+type Model = Error | { user_id: string };
+
+export class AuthenticationMiddleware {
+  constructor(private readonly authorize: Authorize) {}
+
+  async handle({ authorization }: Request): Promise<HttpResponse<Model>> {
+    const isValid = this.validate({ authorization });
+
+    if (!isValid) {
+      return forbidden();
+    }
+
+    try {
+      const user_id = await this.authorize({
+        token: authorization,
+      });
+
+      return ok({ user_id });
+    } catch {
+      return forbidden();
+    }
+  }
+
+  private validate({ authorization }: Request): boolean {
+    const error = new RequiredStringValidator(
+      authorization,
+      'authorization',
+    ).validate();
+
+    return error === undefined;
+  }
+}
--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,45 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HttpResponse, forbidden, ok } from '@/application/helpers';
 import { ForbiddenError } from '@/application/errors';
-import { RequiredStringValidator } from '@/application/validation';
-import { Authorize } from '@/domain/use-cases';
-
-type Request = {
-  authorization: string;
-};
-
-type Model = Error | { user_id: string };
-
-class AuthenticationMiddleware {
-  constructor(private readonly authorize: Authorize) {}
-
-  async handle({ authorization }: Request): Promise<HttpResponse<Model>> {
-    const isValid = this.validate({ authorization });
-
-    if (!isValid) {
-      return forbidden();
-    }
-
-    try {
-      const user_id = await this.authorize({
-        token: authorization,
-      });
-
-      return ok({ user_id });
-    } catch {
-      return forbidden();
-    }
-  }
-
-  private validate({ authorization }: Request): boolean {
-    const error = new RequiredStringValidator(
-      authorization,
-      'authorization',
-    ).validate();
-
-    return error === undefined;
-  }
-}
+import { AuthenticationMiddleware } from '@/application/middlewares';
 
 describe('AuthenticationMiddleware', () => {
   let authorize: jest.Mock;
