1089a35c9d0dd72f792bacc8b01cb0b2e7d8b134
ebertmota <ebert.mota03@gmail.com>
2022-02-14 09:20:10 -0300

    refactor: organize code

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -6,50 +6,58 @@ jest.mock('jsonwebtoken');
 
 describe('JwtTokenGenerator', () => {
   let sut: JwtTokenHandler;
-  let sutParams: TokenGenerator.Params;
   let fakeJwt: jest.Mocked<typeof jwt>;
   let secretKey: string;
 
   beforeAll(() => {
     secretKey = 'any_secret';
     fakeJwt = jwt as jest.Mocked<typeof jwt>;
-    fakeJwt.sign.mockImplementation(() => 'any_token');
-    sutParams = {
-      key: 'any_key',
-      expirationInMs: 1000,
-    };
   });
 
   beforeEach(() => {
     sut = new JwtTokenHandler(secretKey);
   });
 
-  it('should call sign with correct params', async () => {
-    await sut.generateToken(sutParams);
+  describe('generateToken', () => {
+    let sutParams: TokenGenerator.Params;
+    let token: string;
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith(
-      { key: sutParams.key },
-      secretKey,
-      {
-        expiresIn: 1,
-      },
-    );
-  });
-
-  it('should return a token on success', async () => {
-    const token = await sut.generateToken(sutParams);
-
-    expect(token).toBe('any_token');
-  });
-
-  it('should rethrow if sign throws ', async () => {
-    const jwtError = new Error('Jsonwebtoken fails');
-    fakeJwt.sign.mockImplementationOnce(() => {
-      throw jwtError;
+    beforeAll(() => {
+      sutParams = {
+        key: 'any_key',
+        expirationInMs: 1000,
+      };
+      token = 'any_token';
+      fakeJwt.sign.mockImplementation(() => token);
     });
 
-    const promise = sut.generateToken(sutParams);
+    it('should call sign with correct params', async () => {
+      await sut.generateToken(sutParams);
 
-    await expect(promise).rejects.toEqual(jwtError);
+      expect(fakeJwt.sign).toHaveBeenCalledWith(
+        { key: sutParams.key },
+        secretKey,
+        {
+          expiresIn: 1,
+        },
+      );
+    });
+
+    it('should return a token on success', async () => {
+      const generatedToken = await sut.generateToken(sutParams);
+
+      expect(generatedToken).toBe(token);
+    });
+
+    it('should rethrow if sign throws ', async () => {
+      const jwtError = new Error('Jsonwebtoken fails');
+      fakeJwt.sign.mockImplementationOnce(() => {
+        throw jwtError;
+      });
+
+      const promise = sut.generateToken(sutParams);
+
+      await expect(promise).rejects.toEqual(jwtError);
+    });
   });
 });
