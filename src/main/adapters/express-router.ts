f1aa057216d1acf673c92453600782e54271fc69
ebertmota <ebert.mota03@gmail.com>
2022-02-15 09:16:17 -0300

    test: ensure ExpressMiddleware calls handle with correct request

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,8 +5,6 @@ type Adapter = (controller: Controller) => RequestHandler;
 
 export const adaptExpressRoute: Adapter = controller => async (req, res) => {
   const { statusCode, data } = await controller.handle({ ...req.body });
-
   const json = statusCode === 200 ? data : { error: data.message };
-
   res.status(statusCode).json(json);
 };
---
+++ tests/main/adapters/express-middleware.spec.ts
@@ -0,0 +1,48 @@
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { mock, MockProxy } from 'jest-mock-extended';
+import { HttpResponse } from '@/application/helpers';
+
+type Adatper = (middleware: Middleware) => RequestHandler;
+
+const adaptExpressMiddleware: Adatper =
+  middleware => async (req, res, next) => {
+    await middleware.handle({ ...req.headers });
+  };
+
+interface Middleware {
+  handle: (httpRequest: any) => Promise<HttpResponse>;
+}
+
+describe('ExpressMiddleware', () => {
+  let req: Request;
+  let res: Response;
+  let next: NextFunction;
+  let middleware: MockProxy<Middleware>;
+  let sut: RequestHandler;
+
+  beforeAll(() => {
+    req = getMockReq({ headers: { any: 'any' } });
+    res = getMockRes().res;
+    next = getMockRes().next;
+    middleware = mock();
+  });
+
+  beforeEach(() => {
+    sut = adaptExpressMiddleware(middleware);
+  });
+
+  it('should call handle with correct request', async () => {
+    await sut(req, res, next);
+
+    expect(middleware.handle).toHaveBeenCalledWith({ any: 'any' });
+    expect(middleware.handle).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call handle with empty request', async () => {
+    await sut(getMockReq(), res, next);
+
+    expect(middleware.handle).toHaveBeenCalledWith({});
+    expect(middleware.handle).toHaveBeenCalledTimes(1);
+  });
+});
