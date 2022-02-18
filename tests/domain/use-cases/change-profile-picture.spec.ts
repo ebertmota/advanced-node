729a490cb65de95eb00428350d25ee898129ad40
ebertmota <ebert.mota03@gmail.com>
2022-02-18 09:58:01 -0300

    feat: ensure ChangeProfilePicture calls UploadFile with unique key

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 type Input = { id: string; file: Buffer };
 type ChangeProfilePicture = (input: Input) => Promise<void>;
-type Setup = (fileStorage: UploadFile) => ChangeProfilePicture;
+type Setup = (
+  fileStorage: UploadFile,
+  uuidHandler: UUIDGenerator,
+) => ChangeProfilePicture;
 
 const setupChangeProfilePicture: Setup =
-  fileStorage =>
+  (fileStorage, uuidHandler) =>
   async ({ id, file }) => {
     await fileStorage.upload({
       file,
-      key: id,
+      key: uuidHandler.generate({
+        key: id,
+      }),
     });
   };
 
@@ -25,16 +30,33 @@ interface UploadFile {
   upload: (input: UploadFile.Input) => Promise<void>;
 }
 
+namespace UUIDGenerator {
+  export type Input = {
+    key: string;
+  };
+
+  export type Output = string;
+}
+
+interface UUIDGenerator {
+  generate: (input: UUIDGenerator.Input) => UUIDGenerator.Output;
+}
+
 describe('ChangeProfilePicture', () => {
-  let fileStorage: UploadFile;
+  let uuidHandler: MockProxy<UUIDGenerator>;
+  let fileStorage: MockProxy<UploadFile>;
+  let uuid: string;
   let sut: any;
 
   beforeAll(() => {
     fileStorage = mock();
+    uuidHandler = mock();
+    uuid = 'any_uuid';
+    uuidHandler.generate.mockReturnValue(uuid);
   });
 
   beforeEach(() => {
-    sut = setupChangeProfilePicture(fileStorage);
+    sut = setupChangeProfilePicture(fileStorage, uuidHandler);
   });
 
   it('should call UploadFile with correct input', async () => {
@@ -42,7 +64,7 @@ describe('ChangeProfilePicture', () => {
     const id = 'any_id';
     await sut({ id, file });
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: id });
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid });
     expect(fileStorage.upload).toHaveBeenCalledTimes(1);
   });
 });
