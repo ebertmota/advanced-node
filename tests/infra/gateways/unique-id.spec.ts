ef3cb099204a0eaebf4ea2e4685e6c64e00dd323
ebertmota <ebert.mota03@gmail.com>
2022-03-24 09:48:30 -0300

    test: ensure UniqueId returns correct uuid

---
+++ tests/infra/gateways/unique-id.spec.ts
@@ -0,0 +1,32 @@
+import { UniqueId } from '@/infra/crypto';
+
+describe('UniqueId', () => {
+  let key: string;
+  let sut: UniqueId;
+
+  beforeAll(() => {
+    key = 'any_key';
+  });
+
+  beforeEach(() => {
+    sut = new UniqueId();
+  });
+
+  it('should call return correct uuid', () => {
+    const currentDate = new Date(2021, 2, 3, 10, 10, 10);
+    jest.useFakeTimers().setSystemTime(currentDate);
+
+    const result = sut.generate({ key });
+
+    expect(result).toEqual(`${key}_20210303101010`);
+  });
+
+  it('should call return correct uuid', () => {
+    const currentDate = new Date(2018, 2, 10, 18, 1, 0);
+    jest.useFakeTimers().setSystemTime(currentDate);
+
+    const result = sut.generate({ key });
+
+    expect(result).toEqual(`${key}_20180310180100`);
+  });
+});
